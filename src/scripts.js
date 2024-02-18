14afe2c16ff5954e71939685e2adbb298f981223
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-02-17 20:01:40 -0800

    Added searchCity function to have default city

--- src/scripts.js
+++ src/scripts.js
@@ -1,23 +1,27 @@
 function cityTemperature(response) {
   let temp = Math.round(response.data.temperature.current);
-  console.log(temp);
   let tempElement = document.querySelector("#js-temp-element");
   tempElement.innerHTML = temp;
+
+  let cityElement = document.querySelector("#js-city-element");
+  cityElement.innerHTML = response.data.city;
 }
 
-function searchCity(event) {
-  event.preventDefault();
-  let searchInputElement = document.querySelector("#js-search-form-input");
-  let cityElement = document.querySelector("#js-city-element");
-
-  let city = searchInputElement.value;
+function searchCity(city) {
   let apiKey = "1d4e1fa31e8d90b62bfb5t73ob432508";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-
-  cityElement.innerHTML = city;
 
   axios.get(apiUrl).then(cityTemperature);
 }
 
+function searchSubmit(event) {
+  event.preventDefault();
+  let searchInputElement = document.querySelector("#js-search-form-input");
+
+  searchCity(searchInputElement.value);
+}
+
 let searchForm = document.querySelector("#js-search-form");
-searchForm.addEventListener("submit", searchCity);
+searchForm.addEventListener("submit", searchSubmit);
+
+searchCity("Vancouver");
