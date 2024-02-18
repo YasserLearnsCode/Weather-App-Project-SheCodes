d85007aebe85b18d9abb22f36531d4251e49f03a
YasserLearnsCode <147191695+YasserLearnsCode@users.noreply.github.com>
2024-02-17 19:39:02 -0800

    Cleaned up searchCity function and added cityTemperature function

--- src/scripts.js
+++ src/scripts.js
@@ -1,10 +1,23 @@
-function searchWeather(event) {
+function cityTemperature(response) {
+  let temp = Math.round(response.data.temperature.current);
+  console.log(temp);
+  let tempElement = document.querySelector("#js-temp-element");
+  tempElement.innerHTML = temp;
+}
+
+function searchCity(event) {
   event.preventDefault();
-  let searchInputelement = document.querySelector("#js-search-form-input");
-  let cityInput = searchInputelement.value;
-  let cityName = document.querySelector("#js-city-element");
-  cityName.innerHTML = cityInput;
+  let searchInputElement = document.querySelector("#js-search-form-input");
+  let cityElement = document.querySelector("#js-city-element");
+
+  let city = searchInputElement.value;
+  let apiKey = "1d4e1fa31e8d90b62bfb5t73ob432508";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  cityElement.innerHTML = city;
+
+  axios.get(apiUrl).then(cityTemperature);
 }
 
 let searchForm = document.querySelector("#js-search-form");
-searchForm.addEventListener("submit", searchWeather);
+searchForm.addEventListener("submit", searchCity);
